067fa4107643f7c4ce7f1e4b468694229af2c53b
Adil Mian <adilmian95@gmail.com>
2021-08-17 23:52:26 -0400

    plant the debug tree in donezo app

---
+++ app/src/main/java/com/amian/donezo/DonezoApp.kt
@@ -0,0 +1,12 @@
+package com.amian.donezo
+
+import android.app.Application
+import timber.log.Timber
+
+class DonezoApp: Application() {
+
+	override fun onCreate() {
+		super.onCreate()
+		Timber.plant(Timber.DebugTree())
+	}
+}
