dee4ce8c25f1716f3dd3f2f91c04f2c03db62b41
Adil Mian <adilmian95@gmail.com>
2021-08-18 22:08:17 -0400

    added onboarding fragment + screen fragments

---
+++ app/src/main/java/com/amian/donezo/views/onboarding/OnboardingFragment.kt
@@ -0,0 +1,44 @@
+package com.amian.donezo.views.onboarding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.amian.donezo.databinding.FragmentOnboardingBinding
+
+class OnboardingFragment : Fragment() {
+
+	private var _binding: FragmentOnboardingBinding? = null
+	private val binding
+		get() = _binding!!
+
+
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		_binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+
+		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		binding.pager.adapter = OnboardingPagerAdapter(this)
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		_binding = null
+	}
+}
+
+class OnboardingPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+	override fun getItemCount() = 3
+
+	override fun createFragment(position: Int) = OnboardingScreenFragment.newInstance(position)
+}
---
+++ app/src/main/java/com/amian/donezo/views/onboarding/OnboardingScreenFragment.kt
@@ -0,0 +1,50 @@
+package com.amian.donezo.views.onboarding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import com.amian.donezo.databinding.OnboardingFinalScreenBinding
+import com.amian.donezo.databinding.OnboardingMiddleScreenBinding
+import com.amian.donezo.databinding.OnboardingStartScreenBinding
+
+class OnboardingScreenFragment : Fragment() {
+
+	private val position by lazy {
+		requireArguments().getInt(POSITION_KEY)
+	}
+
+
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+
+		return when (position) {
+			0 -> OnboardingStartScreenBinding.inflate(inflater, container, false).root
+			1 -> OnboardingMiddleScreenBinding.inflate(inflater, container, false).root
+			2 -> {
+				with(OnboardingFinalScreenBinding.inflate(inflater, container, false)){
+
+					button.setOnClickListener {
+					}
+
+					root
+				}}
+			else -> super.onCreateView(inflater, container, savedInstanceState)
+		}
+	}
+
+	companion object {
+		private const val POSITION_KEY = "position"
+
+		fun newInstance(position: Int) = OnboardingScreenFragment().apply {
+			arguments = bundleOf(
+				POSITION_KEY to position
+			)
+		}
+	}
+}
