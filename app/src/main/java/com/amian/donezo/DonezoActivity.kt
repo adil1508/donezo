30c534d5bab74a1192101c400bde5b2930c3e73c
Adil Mian <adilmian95@gmail.com>
2021-08-23 21:36:19 -0400

    renamed main activity

--- app/src/main/java/com/amian/donezo/DonezoActivity.kt
+++ app/src/main/java/com/amian/donezo/DonezoActivity.kt
@@ -3,7 +3,7 @@ package com.amian.donezo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class DonezoActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
