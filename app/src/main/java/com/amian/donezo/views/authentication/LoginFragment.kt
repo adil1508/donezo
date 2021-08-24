d3fc4a9d34b274a857a97865f0379317c56a7a3a
Adil Mian <adilmian95@gmail.com>
2021-08-24 19:20:37 -0400

    login fragment leads to signup

--- app/src/main/java/com/amian/donezo/views/authentication/LoginFragment.kt
+++ app/src/main/java/com/amian/donezo/views/authentication/LoginFragment.kt
@@ -5,13 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.amian.donezo.databinding.FragmentLoginBinding
 
-class LoginFragment: Fragment() {
+class LoginFragment : Fragment() {
 
 	private var _binding: FragmentLoginBinding? = null
 	private val binding
 		get() = _binding!!
+
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
@@ -19,6 +21,11 @@ class LoginFragment: Fragment() {
 	): View {
 
 		_binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+		binding.loginButton.setOnClickListener {
+			findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
+		}
+
 		return binding.root
 	}
 }
