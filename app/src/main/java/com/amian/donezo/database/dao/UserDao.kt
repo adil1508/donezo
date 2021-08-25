a387d415e4ba33d91957565725d17d1ef34f6085
Adil Mian <adilmian95@gmail.com>
2021-08-24 21:04:54 -0400

    added get method to dao + set table name

--- app/src/main/java/com/amian/donezo/database/dao/UserDao.kt
+++ app/src/main/java/com/amian/donezo/database/dao/UserDao.kt
@@ -4,8 +4,11 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Update
 import com.amian.donezo.database.entities.User
+import com.amian.donezo.database.entities.User.Companion.USERS_TABLE
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 abstract class UserDao {
@@ -18,5 +21,8 @@ abstract class UserDao {
 
 	@Update
 	abstract suspend fun updateUser(user: User)
+
+	@Query("SELECT * FROM $USERS_TABLE LIMIT 1")
+	abstract fun getCurrentUser(): Flow<User>
 
 }
