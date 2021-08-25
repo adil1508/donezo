e4544b46c80545068b178a8d433d2b74b857b134
Adil Mian <adilmian95@gmail.com>
2021-08-24 20:00:30 -0400

    added user entity

---
+++ app/src/main/java/com/amian/donezo/database/entities/User.kt
@@ -0,0 +1,11 @@
+package com.amian.donezo.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+	@PrimaryKey(autoGenerate = true) val id: Int,
+	val name: String,
+	val email: String
+)
