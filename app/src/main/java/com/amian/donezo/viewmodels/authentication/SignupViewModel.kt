19c540346ba68e9fc120efdc95e17b66fffa384c
Adil Mian <adilmian95@gmail.com>
2021-08-25 01:10:19 -0400

    added signup viewmodel

---
+++ app/src/main/java/com/amian/donezo/viewmodels/authentication/SignupViewModel.kt
@@ -0,0 +1,35 @@
+package com.amian.donezo.viewmodels.authentication
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.amian.donezo.repositories.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignupViewModel @Inject constructor(
+	private val userRepo: UserRepository,
+) : ViewModel() {
+
+	private val currentUser = userRepo.observeUser()
+
+	val authenticated = MutableLiveData(false)
+
+	init {
+		viewModelScope.launch {
+			currentUser.distinctUntilChanged().collect {
+				if (it != null) authenticated.value = true
+			}
+		}
+	}
+
+	fun signUp(name: String, email: String, password: String) =
+		viewModelScope.launch {
+			userRepo.signup(name = name, email = email, password = password)
+		}
+
+}
