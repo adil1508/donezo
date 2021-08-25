b0fad0180bff5c21e73ddbef36c356237ebddb7f
Adil Mian <adilmian95@gmail.com>
2021-08-24 21:13:18 -0400

    dependency injection is working in viewmodel

--- app/src/main/java/com/amian/donezo/viewmodels/authentication/LoginViewModel.kt
+++ app/src/main/java/com/amian/donezo/viewmodels/authentication/LoginViewModel.kt
@@ -1,9 +1,16 @@
 package com.amian.donezo.viewmodels.authentication
 
 import androidx.lifecycle.ViewModel
+import com.amian.donezo.repositories.UserRepoImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
+import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel : ViewModel() {
+class LoginViewModel @Inject constructor(private val userRepo: UserRepoImpl) : ViewModel() {
+
+	fun test() {
+		Timber.d("Huzzah!")
+	}
 
 }
--- app/src/main/java/com/amian/donezo/views/authentication/LoginFragment.kt
+++ app/src/main/java/com/amian/donezo/views/authentication/LoginFragment.kt
@@ -5,14 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.amian.donezo.databinding.FragmentLoginBinding
+import com.amian.donezo.viewmodels.authentication.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
 
 	private var _binding: FragmentLoginBinding? = null
 	private val binding
 		get() = _binding!!
+
+	private val viewModel: LoginViewModel by viewModels()
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
@@ -23,7 +29,8 @@ class LoginFragment : Fragment() {
 		_binding = FragmentLoginBinding.inflate(inflater, container, false)
 
 		binding.loginButton.setOnClickListener {
-			findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
+			viewModel.test()
+//			findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
 		}
 
 		return binding.root
