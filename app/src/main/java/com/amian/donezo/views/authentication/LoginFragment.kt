88465ae29e3e631d0b1b6f065b961f856f81d8e7
Adil Mian <adilmian95@gmail.com>
2021-08-24 21:30:48 -0400

    setup basic login functionality to test flow

--- app/src/main/java/com/amian/donezo/views/authentication/LoginFragment.kt
+++ app/src/main/java/com/amian/donezo/views/authentication/LoginFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.amian.donezo.AuthenticationNavigationDirections
 import com.amian.donezo.databinding.FragmentLoginBinding
 import com.amian.donezo.viewmodels.authentication.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,9 +30,12 @@ class LoginFragment : Fragment() {
 		_binding = FragmentLoginBinding.inflate(inflater, container, false)
 
 		binding.loginButton.setOnClickListener {
-			viewModel.test()
-//			findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
+			viewModel.login("test", "password")
 		}
+
+		viewModel.authenticated.observe(requireActivity(), {
+			if (it) findNavController().navigate(AuthenticationNavigationDirections.actionAuthenticated())
+		})
 
 		return binding.root
 	}
