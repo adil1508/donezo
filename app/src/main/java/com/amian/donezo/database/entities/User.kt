5d7566d46bc0d8f93a1d622090a3d3f5e8c211f7
Adil Mian <adilmian95@gmail.com>
2021-08-24 21:04:54 -0400

    added get method to dao + set table name

--- app/src/main/java/com/amian/donezo/database/entities/User.kt
+++ app/src/main/java/com/amian/donezo/database/entities/User.kt
@@ -2,12 +2,17 @@ package com.amian.donezo.database.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.amian.donezo.database.entities.User.Companion.USERS_TABLE
 
-@Entity
+@Entity(tableName = USERS_TABLE)
 data class User(
 	@PrimaryKey(autoGenerate = true) val id: Int,
 	val name: String,
 	val email: String
 ) {
 	constructor(name: String, email: String) : this(0, name, email)
+
+	companion object {
+		const val USERS_TABLE = "Users"
+	}
 }
