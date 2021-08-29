3e506a5b4a5c85d7a9b8abd6c8728a69e4f00a45
Adil Mian <adilmian95@gmail.com>
2021-08-29 15:49:38 -0400

    set start destination as home fragment

--- app/src/main/java/com/amian/donezo/views/HomeFragment.kt
+++ app/src/main/java/com/amian/donezo/views/HomeFragment.kt
@@ -5,20 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.amian.donezo.ApplicationNavigationDirections
 import com.amian.donezo.databinding.FragmentHomeBinding
+import com.amian.donezo.repositories.UserRepository
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
 	private var _binding: FragmentHomeBinding? = null
 	val binding
 		get() = _binding!!
 
+	@Inject
+	lateinit var userRepository: UserRepository
+
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View {
+
+		runBlocking {
+			if (userRepository.observeUser().first() == null) findNavController().navigate(
+				ApplicationNavigationDirections.actionUnauthenticated()
+			)
+		}
+
 		_binding = FragmentHomeBinding.inflate(inflater, container, false)
 		return binding.root
 	}
