e5e2400204a1d1b238266a4b998ee9da8e6c7d5c
Adil Mian <adilmian95@gmail.com>
2021-08-29 18:01:31 -0400

    added viewmodel and buttons handling

--- app/src/main/java/com/amian/donezo/views/authentication/ResetPasswordFragment.kt
+++ app/src/main/java/com/amian/donezo/views/authentication/ResetPasswordFragment.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.amian.donezo.databinding.FragmentResetPasswordBinding
+import com.amian.donezo.viewmodels.authentication.ResetPasswordViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class ResetPasswordFragment: Fragment() {
+@AndroidEntryPoint
+class ResetPasswordFragment : Fragment() {
 
 	private var _binding: FragmentResetPasswordBinding? = null
 	private val binding
 		get() = _binding!!
+
+	private val viewModel: ResetPasswordViewModel by viewModels()
 
 
 	override fun onCreateView(
@@ -20,6 +28,18 @@ class ResetPasswordFragment: Fragment() {
 		savedInstanceState: Bundle?
 	): View {
 		_binding = FragmentResetPasswordBinding.inflate(inflater, container, false)
+
+		viewModel.resetEmailSent.observe(viewLifecycleOwner) { sent ->
+			if (sent) {
+				Toast.makeText(requireContext(), "Email sent to reset password", Toast.LENGTH_LONG).show()
+				findNavController().navigate(ResetPasswordFragmentDirections.actionResetPasswordFragmentToLoginFragment())
+			}
+		}
+
+		binding.resetPasswordButton.setOnClickListener {
+			viewModel.resetPassword(binding.emailInput.editText!!.text.toString())
+		}
+
 		return binding.root
 	}
 }
