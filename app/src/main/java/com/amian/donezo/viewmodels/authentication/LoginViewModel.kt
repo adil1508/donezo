f956f112fcf043a92d81ea69e79445268ea530c9
Adil Mian <adilmian95@gmail.com>
2021-08-30 21:34:53 -0400

    login input validated

--- app/src/main/java/com/amian/donezo/viewmodels/authentication/LoginViewModel.kt
+++ app/src/main/java/com/amian/donezo/viewmodels/authentication/LoginViewModel.kt
@@ -7,7 +7,6 @@ import com.amian.donezo.repositories.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,6 +17,12 @@ class LoginViewModel @Inject constructor(private val userRepo: UserRepository) :
 
 	val authenticated = MutableLiveData(false)
 
+	val emailError = MutableLiveData<String?>(null)
+	val passwordError = MutableLiveData<String?>(null)
+
+	val email = MutableLiveData<String?>()
+	val password = MutableLiveData<String?>()
+
 	init {
 		viewModelScope.launch {
 			currentUser.distinctUntilChanged().collect {
@@ -26,8 +31,39 @@ class LoginViewModel @Inject constructor(private val userRepo: UserRepository) :
 		}
 	}
 
-	fun login(email: String, password: String) = viewModelScope.launch {
-		userRepo.login(email = email, password = password)
+	fun login() {
+
+		// Validate inputs
+		validateEmail(email = email.value)
+		validatePassword(password = password.value)
+
+		if (emailError.value == null && passwordError.value == null) viewModelScope.launch {
+			userRepo.login(email = email.value!!, password = password.value ?: "")
+		}
+
 	}
+
+	private fun validateEmail(email: String?) {
+		if (email.isNullOrBlank()) {
+			emailError.value = "Email is required"
+			return
+		}
+		if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+			emailError.value = "Email is not valid"
+			return
+		}
+
+		emailError.value = null
+	}
+
+	private fun validatePassword(password: String?) {
+		if (password.isNullOrBlank()) {
+			passwordError.value = "Password is required"
+			return
+		}
+
+		passwordError.value = null
+	}
+
 
 }
