fc855b19758414af0d66d38a5ee3e492ff7205b6
Adil Mian <adilmian95@gmail.com>
2021-09-02 20:32:39 -0400

    added todo database entity

---
+++ app/src/main/java/com/amian/donezo/database/entities/Todo.kt
@@ -0,0 +1,15 @@
+package com.amian.donezo.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.amian.donezo.database.entities.Todo.Companion.TODO_TABLE
+
+@Entity(tableName = TODO_TABLE)
+data class Todo (
+	@PrimaryKey val email: String,
+	val todo: String
+) {
+	companion object {
+		const val TODO_TABLE = "Todos"
+	}
+}
