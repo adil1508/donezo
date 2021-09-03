ace3f1a88a327a2df8c8f64b126e9449a6e3c799
Adil Mian <adilmian95@gmail.com>
2021-09-02 20:18:42 -0400

    added basic add too modal

---
+++ app/src/main/java/com/amian/donezo/views/AddTodoFragment.kt
@@ -0,0 +1,26 @@
+package com.amian.donezo.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.amian.donezo.databinding.FragmentAddTodoBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class AddTodoFragment : BottomSheetDialogFragment() {
+
+	private var _binding: FragmentAddTodoBinding? = null
+	val binding: FragmentAddTodoBinding
+		get() = _binding!!
+
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+
+		_binding = FragmentAddTodoBinding.inflate(inflater, container, false)
+
+		return binding.root
+	}
+}
