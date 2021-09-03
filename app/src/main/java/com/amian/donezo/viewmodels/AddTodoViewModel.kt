74e434767975bcf66f9396f7ea78a75eb1fd1125
Adil Mian <adilmian95@gmail.com>
2021-09-02 20:29:44 -0400

    added empty viewmodel

---
+++ app/src/main/java/com/amian/donezo/viewmodels/AddTodoViewModel.kt
@@ -0,0 +1,13 @@
+package com.amian.donezo.viewmodels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class AddTodoViewModel: ViewModel() {
+
+	/*
+	* TODO:
+	*  - Add method to write to database + write to firestore
+	* */
+}
