1940f046a684c5bcff0609f6c1f701b22bf189b7
Adil Mian <adilmian95@gmail.com>
2021-09-02 20:38:01 -0400

    setup todo entity in database and setup provider

--- app/src/main/java/com/amian/donezo/database/DonezoDatabase.kt
+++ app/src/main/java/com/amian/donezo/database/DonezoDatabase.kt
@@ -2,10 +2,13 @@ package com.amian.donezo.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.amian.donezo.database.dao.TodoDao
 import com.amian.donezo.database.dao.UserDao
+import com.amian.donezo.database.entities.Todo
 import com.amian.donezo.database.entities.User
 
-@Database(entities = [User::class], version = 1, exportSchema = true)
+@Database(entities = [User::class, Todo::class], version = 1, exportSchema = true)
 abstract class DonezoDatabase: RoomDatabase() {
 	abstract fun userDao(): UserDao
+	abstract fun todoDao(): TodoDao
 }
