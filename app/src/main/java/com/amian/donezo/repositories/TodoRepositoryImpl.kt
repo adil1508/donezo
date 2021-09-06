1091730d6e27a7b7f5e32a3a8271ce8bd706984b
Adil Mian <adilmian95@gmail.com>
2021-09-06 16:28:51 -0400

    slight refactor

--- app/src/main/java/com/amian/donezo/repositories/TodoRepositoryImpl.kt
+++ app/src/main/java/com/amian/donezo/repositories/TodoRepositoryImpl.kt
@@ -8,34 +8,35 @@ import javax.inject.Inject
 
 class TodoRepositoryImpl @Inject constructor(
 	private val todoDao: TodoDao,
-	private val firestore: FirebaseFirestore
+	firestore: FirebaseFirestore
 ) : TodoRepository {
+
+	private val usersCollection = firestore.collection("users")
 
 	override suspend fun addTodo(todo: Todo) {
 		todoDao.insertTodo(todo)
-		addToFirestore(todo)
+		addTodoToFirestore(todo)
 	}
 
-	private suspend fun addToFirestore(todo: Todo) {
-
-		firestore.collection("users").whereEqualTo("email", todo.email).limit(1).get()
+	private fun addTodoToFirestore(todo: Todo) =
+		usersCollection.whereEqualTo("email", todo.email).limit(1).get()
 			.addOnCompleteListener {
 				if (it.isSuccessful) {
 					it.result?.documents?.first()?.let { userDoc ->
 						Timber.d("Successfully got document for user: ${todo.email}")
-						firestore.collection("users").document(userDoc.id).collection("todos").add(
-							hashMapOf("msg" to todo.todo)
-						).addOnCompleteListener { lastTask ->
-							if (lastTask.isSuccessful) {
-								Timber.d("Successfully wrote todo to firestore")
+						usersCollection.document(userDoc.id).collection("todos")
+							.add(
+								hashMapOf("msg" to todo.todo)
+							).addOnCompleteListener { lastTask ->
+								if (lastTask.isSuccessful) {
+									Timber.d("Successfully wrote todo to firestore")
+								}
 							}
-						}
 					}
 				} else {
 					Timber.d("Failed to get document for user: ${todo.email}")
 				}
 			}
 
-		firestore.collection("users")
-	}
+
 }
