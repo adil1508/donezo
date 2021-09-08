cb58954a4731e29220d85924d7ecfdf8e1452031
Adil Mian <adilmian95@gmail.com>
2021-09-07 21:42:48 -0400

    added id to Todo entity

--- app/src/main/java/com/amian/donezo/database/entities/Todo.kt
+++ app/src/main/java/com/amian/donezo/database/entities/Todo.kt
@@ -6,9 +6,11 @@ import com.amian.donezo.database.entities.Todo.Companion.TODO_TABLE
 
 @Entity(tableName = TODO_TABLE)
 data class Todo(
-	@PrimaryKey val email: String,
+	val email: String,
 	val todo: String
 ) {
+	@PrimaryKey(autoGenerate = true) var id: Long = 0
+
 	companion object {
 		const val TODO_TABLE = "Todos"
 	}
