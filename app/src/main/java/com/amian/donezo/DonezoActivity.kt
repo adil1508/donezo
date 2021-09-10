4d378afbd6dc3871ae5aae0873ef494f0b4933f7
Adil Mian <adilmian95@gmail.com>
2021-09-09 20:25:08 -0400

    inject user repo in activity to get user flow started"

--- app/src/main/java/com/amian/donezo/DonezoActivity.kt
+++ app/src/main/java/com/amian/donezo/DonezoActivity.kt
@@ -2,10 +2,17 @@ package com.amian.donezo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.amian.donezo.repositories.UserRepository
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class DonezoActivity : AppCompatActivity() {
+
+	@Inject
+	lateinit var userRepository: UserRepository
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
