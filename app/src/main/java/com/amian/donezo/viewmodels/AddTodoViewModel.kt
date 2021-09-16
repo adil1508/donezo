9455588b5ec39a94ee77d9472eefa1faa0e43f1a
Adil Mian <adilmian95@gmail.com>
2021-09-16 14:48:02 -0400

    updated add todo

--- app/src/main/java/com/amian/donezo/viewmodels/AddTodoViewModel.kt
+++ app/src/main/java/com/amian/donezo/viewmodels/AddTodoViewModel.kt
@@ -2,8 +2,6 @@ package com.amian.donezo.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import com.amian.donezo.database.entities.Todo
 import com.amian.donezo.repositories.TodoRepository
 import com.amian.donezo.repositories.UserRepository
@@ -16,17 +14,15 @@ import javax.inject.Inject
 @HiltViewModel
 class AddTodoViewModel @Inject constructor(
 	private val todoRepo: TodoRepository,
-	userRepo: UserRepository
+	private val userRepo: UserRepository
 ) : ViewModel() {
 
 	val todoText = MutableLiveData("")
 
-	val currentUserLiveData = userRepo.observeUser().asLiveData()
-
-	fun addTodo() = currentUserLiveData.value?.let { user ->
-		todoText.value.takeIf { !it.isNullOrBlank() }.let { todoTextVal ->
+	fun addTodo() = userRepo.currentUser.value?.let { user ->
+		todoText.value.takeIf { !it.isNullOrBlank() }?.let { todoTextVal ->
 			GlobalScope.launch(Dispatchers.IO) {
-				todoRepo.addTodo(Todo(email = user.email, todo = todoTextVal!!))
+				todoRepo.addTodo(Todo(email = user.email, todo = todoTextVal))
 			}
 			// reset it's value
 			todoText.value = ""
