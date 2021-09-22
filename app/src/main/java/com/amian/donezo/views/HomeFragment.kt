cee676733ed2e4c64b7f695eee27163f3f0e81a2
Adil Mian <adilmian95@gmail.com>
2021-09-22 19:42:21 -0400

    added click listener to checkbox

--- app/src/main/java/com/amian/donezo/views/HomeFragment.kt
+++ app/src/main/java/com/amian/donezo/views/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.amian.donezo.views
 
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -7,6 +8,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -135,6 +137,20 @@ class HomeFragment : Fragment() {
 
             fun bind(todo: Todo) {
                 binding.text.text = todo.todo
+                binding.checkbox.setOnClickListener { checkbox ->
+                    when((checkbox as CheckBox).isChecked) {
+                        true -> {
+                            binding.text.apply {
+                                paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                            }
+                        }
+                        false -> {
+                            binding.text.apply {
+                                paintFlags = paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+                            }
+                        }
+                    }
+                }
             }
         }
     }
