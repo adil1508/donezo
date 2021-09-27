a241c0c77264200014e67a88f9118f6989a86cd4
Adil Mian <adilmian95@gmail.com>
2021-09-27 11:41:39 -0400

    Added constants file

---
+++ app/src/main/java/com/amian/donezo/Constants.kt
@@ -0,0 +1,10 @@
+package com.amian.donezo
+
+class Constants {
+    companion object {
+        const val FIRESTORE_USERS_TABLE = "users"
+        const val FIRESTORE_USERS_NAME_KEY = "name"
+        const val FIRESTORE_USERS_EMAIL_KEY = "email"
+        const val FIRESTORE_USERS_TODOS_TABLE = "todos"
+    }
+}
