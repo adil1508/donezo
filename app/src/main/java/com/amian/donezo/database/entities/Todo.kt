8842694297ec56bdfb352a183d61abbb35aa0478
Adil Mian <adilmian95@gmail.com>
2021-09-29 19:41:21 -0400

    added done boolean to Todo entity

--- app/src/main/java/com/amian/donezo/database/entities/Todo.kt
+++ app/src/main/java/com/amian/donezo/database/entities/Todo.kt
@@ -6,12 +6,14 @@ import com.amian.donezo.database.entities.Todo.Companion.TODO_TABLE
 
 @Entity(tableName = TODO_TABLE)
 data class Todo(
-	val email: String,
-	val todo: String
+    val email: String,
+    val todo: String,
+    val done: Boolean = false
 ) {
-	@PrimaryKey(autoGenerate = true) var id: Long = 0
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
 
-	companion object {
-		const val TODO_TABLE = "Todos"
-	}
+    companion object {
+        const val TODO_TABLE = "Todos"
+    }
 }
