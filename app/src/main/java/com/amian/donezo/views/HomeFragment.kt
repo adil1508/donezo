c3bea74bbb724993d04c1144d04fa2f32394d6cb
Adil Mian <adilmian95@gmail.com>
2021-10-04 19:58:03 -0400

    removed unused import

--- app/src/main/java/com/amian/donezo/views/HomeFragment.kt
+++ app/src/main/java/com/amian/donezo/views/HomeFragment.kt
@@ -9,7 +9,6 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
