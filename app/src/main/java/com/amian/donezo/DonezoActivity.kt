8e099128892df283e75f60055ed3ad34d3a09ece
Adil Mian <adilmian95@gmail.com>
2021-10-04 20:43:47 -0400

    tapping on 'home' icon opens up navigation drawer

--- app/src/main/java/com/amian/donezo/DonezoActivity.kt
+++ app/src/main/java/com/amian/donezo/DonezoActivity.kt
@@ -2,24 +2,45 @@ package com.amian.donezo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.amian.donezo.databinding.ActivityMainBinding
 import com.amian.donezo.repositories.UserRepository
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class DonezoActivity : AppCompatActivity() {
 
-	@Inject
-	lateinit var userRepository: UserRepository
+    @Inject
+    lateinit var userRepository: UserRepository
 
-	lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		binding = ActivityMainBinding.inflate(layoutInflater)
-		setSupportActionBar(binding.toolbar)
-		setContentView(binding.root)
-	}
+    lateinit var appBarConfig: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        // Setup navigation controller
+        val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        appBarConfig = AppBarConfiguration(setOf(R.id.homeFragment), binding.drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfig)
+        binding.navView.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navigationController = findNavController(R.id.nav_host_fragment)
+        return navigationController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
+    }
+
 }
