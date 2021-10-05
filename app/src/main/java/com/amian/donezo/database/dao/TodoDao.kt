9daa7430a3cb2a91fc6958d5249670f4a9a63a21
Adil Mian <adilmian95@gmail.com>
2021-10-04 21:13:26 -0400

    refactored the repo

--- app/src/main/java/com/amian/donezo/database/dao/TodoDao.kt
+++ app/src/main/java/com/amian/donezo/database/dao/TodoDao.kt
@@ -11,18 +11,18 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TodoDao {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertTodo(todo: Todo): Long
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun insertTodo(todo: Todo): Long
 
-    @Delete
-    suspend fun deleteTodo(todo: Todo)
+	@Delete
+	suspend fun deleteTodo(todo: Todo)
 
-    @Query("UPDATE ${Todo.TODO_TABLE} SET done=:done WHERE id=:id")
-    suspend fun markTodoDone(id: Long, done: Boolean)
+	@Query("UPDATE ${Todo.TODO_TABLE} SET done=:done WHERE id=:id")
+	suspend fun markTodoDone(id: Long, done: Boolean)
 
-    @Query("SELECT * FROM ${Todo.TODO_TABLE} WHERE email = :email ORDER BY id desc")
-    fun observeTodos(email: String): Flow<List<Todo>>
+	@Query("SELECT * FROM ${Todo.TODO_TABLE} WHERE email = :email ORDER BY id desc")
+	fun observeTodos(email: String): Flow<List<Todo>>
 
-    @Query("DELETE FROM ${Todo.TODO_TABLE}")
-    fun deleteAllTodos()
+	@Query("DELETE FROM ${Todo.TODO_TABLE}")
+	fun deleteAllTodos()
 }
--- app/src/main/java/com/amian/donezo/repositories/UserRepoImpl.kt
+++ app/src/main/java/com/amian/donezo/repositories/UserRepoImpl.kt
@@ -16,77 +16,77 @@ import timber.log.Timber
 import javax.inject.Inject
 
 class UserRepoImpl @Inject constructor(
-    private val userDao: UserDao,
-    private val firebaseAuth: FirebaseAuth,
-    firestore: FirebaseFirestore
+	private val userDao: UserDao,
+	private val firebaseAuth: FirebaseAuth,
+	firestore: FirebaseFirestore
 ) : UserRepository {
 
-    override val currentUser =
-        userDao.observeCurrentUser().stateIn(GlobalScope, SharingStarted.Eagerly, null)
+	override val currentUser =
+		userDao.observeCurrentUser().stateIn(GlobalScope, SharingStarted.Eagerly, null)
 
-    private val usersCollection = firestore.collection(FIRESTORE_USERS_TABLE)
+	private val usersCollection = firestore.collection(FIRESTORE_USERS_TABLE)
 
-    override suspend fun clearUser() = userDao.deleteUser()
+	override suspend fun clearUser() = userDao.deleteUser()
 
-    override suspend fun login(email: String, password: String) {
-        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
-            if (it.isSuccessful) {
-                GlobalScope.launch(Dispatchers.IO) {
-                    getUserInfoFromFirestore(email = email)
-                }
-            } else {
-                Timber.e("Could not login user!")
-            }
-        }
-    }
+	override suspend fun login(email: String, password: String) {
+		firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+			if (it.isSuccessful) {
+				GlobalScope.launch(Dispatchers.IO) {
+					getUserInfoFromFirestore(email = email)
+				}
+			} else {
+				Timber.e("Could not login user!")
+			}
+		}
+	}
 
-    override suspend fun signup(name: String, email: String, password: String) {
-        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-            if (it.isSuccessful) {
-                GlobalScope.launch(Dispatchers.IO) {
-                    setUserInfoInFirestore(email = email, name = name)
-                }
-            } else {
-                Timber.e("Could not sign up user!")
-            }
-        }
-    }
+	override suspend fun signup(name: String, email: String, password: String) {
+		firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+			if (it.isSuccessful) {
+				GlobalScope.launch(Dispatchers.IO) {
+					setUserInfoInFirestore(email = email, name = name)
+				}
+			} else {
+				Timber.e("Could not sign up user!")
+			}
+		}
+	}
 
-    private suspend fun setUser(name: String, email: String) =
-        userDao.insertUser(User(email = email, name = name))
+	private suspend fun setUser(name: String, email: String) =
+		userDao.insertUser(User(email = email, name = name))
 
-    private suspend fun getUserInfoFromFirestore(email: String) {
-        usersCollection.whereEqualTo(FIRESTORE_USERS_EMAIL_KEY, email)
-            .limit(1).get()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // write to local db
-                    task.result?.documents?.let {
-                        val document = it[0]
-                        GlobalScope.launch(Dispatchers.IO) {
-                            setUser(
-                                email = email,
-                                name = document.getString(FIRESTORE_USERS_NAME_KEY)!!
-                            )
-                        }
-                    }
-                } else {
-                    Timber.e("Failed to retrieve user info from Firestore!")
-                }
-            }
-    }
+	private suspend fun getUserInfoFromFirestore(email: String) {
+		usersCollection.whereEqualTo(FIRESTORE_USERS_EMAIL_KEY, email)
+			.limit(1).get()
+			.addOnCompleteListener { task ->
+				if (task.isSuccessful) {
+					// write to local db
+					task.result?.documents?.let {
+						val document = it[0]
+						GlobalScope.launch(Dispatchers.IO) {
+							setUser(
+								email = email,
+								name = document.getString(FIRESTORE_USERS_NAME_KEY)!!
+							)
+						}
+					}
+				} else {
+					Timber.e("Failed to retrieve user info from Firestore!")
+				}
+			}
+	}
 
-    private suspend fun setUserInfoInFirestore(email: String, name: String) {
-        usersCollection
-            .add(hashMapOf(FIRESTORE_USERS_NAME_KEY to name, FIRESTORE_USERS_EMAIL_KEY to email))
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    GlobalScope.launch(Dispatchers.IO) {
-                        setUser(name = name, email = email)
-                    }
-                } else {
-                    Timber.e("Could not write user to Firestore!")
-                }
-            }
-    }
+	private suspend fun setUserInfoInFirestore(email: String, name: String) {
+		usersCollection
+			.add(hashMapOf(FIRESTORE_USERS_NAME_KEY to name, FIRESTORE_USERS_EMAIL_KEY to email))
+			.addOnCompleteListener {
+				if (it.isSuccessful) {
+					GlobalScope.launch(Dispatchers.IO) {
+						setUser(name = name, email = email)
+					}
+				} else {
+					Timber.e("Could not write user to Firestore!")
+				}
+			}
+	}
 }
