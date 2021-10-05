a3775deecea21de360ab0a89c9f6dd3eea1354a7
Adil Mian <adilmian95@gmail.com>
2021-10-04 21:05:19 -0400

    line ending change

--- app/src/main/java/com/amian/donezo/DonezoActivity.kt
+++ app/src/main/java/com/amian/donezo/DonezoActivity.kt
@@ -16,34 +16,34 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class DonezoActivity : AppCompatActivity() {
 
-    // Injecting this here to start the user flow
-    @Inject
-    lateinit var userRepository: UserRepository
+	// Injecting this here to start the user flow
+	@Inject
+	lateinit var userRepository: UserRepository
 
-    private lateinit var binding: ActivityMainBinding
+	private lateinit var binding: ActivityMainBinding
 
-    private lateinit var appBarConfig: AppBarConfiguration
+	private lateinit var appBarConfig: AppBarConfiguration
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setupNavController()
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+		setupNavController()
+	}
 
-    override fun onSupportNavigateUp() =
-        findNavController(R.id.nav_host_fragment).navigateUp(appBarConfig) || super.onSupportNavigateUp()
+	override fun onSupportNavigateUp() =
+		findNavController(R.id.nav_host_fragment).navigateUp(appBarConfig) || super.onSupportNavigateUp()
 
 
-    private fun setupNavController() {
-        setSupportActionBar(binding.toolbar)
+	private fun setupNavController() {
+		setSupportActionBar(binding.toolbar)
 
-        val navController =
-            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
-        appBarConfig = AppBarConfiguration(setOf(R.id.homeFragment), binding.drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfig)
-        binding.navView.setupWithNavController(navController)
+		val navController =
+			(supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+		appBarConfig = AppBarConfiguration(setOf(R.id.homeFragment), binding.drawerLayout)
+		setupActionBarWithNavController(navController, appBarConfig)
+		binding.navView.setupWithNavController(navController)
 
-    }
+	}
 
 }
