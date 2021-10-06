9b2ac671560b1bc1b95cb7ba13e98bb7f86662f8
Adil Mian <adilmian95@gmail.com>
2021-10-06 19:30:25 -0400

    setup handler in layout

--- app/src/main/java/com/amian/donezo/views/AboutFragment.kt
+++ app/src/main/java/com/amian/donezo/views/AboutFragment.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.amian.donezo.databinding.FragmentAboutBinding
 
-class AboutFragment: Fragment(), versionHandler{
+class AboutFragment : Fragment(), versionHandler {
 
 	private var _binding: FragmentAboutBinding? = null
 	private val binding: FragmentAboutBinding
@@ -18,8 +18,9 @@ class AboutFragment: Fragment(), versionHandler{
 		inflater: LayoutInflater,
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
-	): View? {
+	): View {
 		_binding = FragmentAboutBinding.inflate(inflater, container, false)
+		binding.versionHandler = this
 		return binding.root
 	}
 
@@ -28,10 +29,11 @@ class AboutFragment: Fragment(), versionHandler{
 		_binding = null
 	}
 
-	override fun getVersionCode(context: Context) = context.packageManager.getPackageInfo(context.packageName, 0).longVersionCode
+	override fun getVersionCode(context: Context): String =
+		context.packageManager.getPackageInfo(context.packageName, 0).versionName
 
 }
 
 interface versionHandler {
-	fun getVersionCode(context: Context): Long
+	fun getVersionCode(context: Context): String
 }
